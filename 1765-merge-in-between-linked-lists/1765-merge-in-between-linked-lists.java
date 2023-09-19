f498675a919b9726013f436f6c45fb61f40c73c5
Jasleengujral01 <122670620+Jasleengujral01@users.noreply.github.com>
2023-09-19 21:55:41 +0530

    Time: 1 ms (100%), Space: 46.6 MB (49.36%) - LeetHub

--- 1765-merge-in-between-linked-lists/1765-merge-in-between-linked-lists.java
+++ 1765-merge-in-between-linked-lists/1765-merge-in-between-linked-lists.java
@@ -10,21 +10,28 @@
  */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-       ListNode dummy=new ListNode(-1);
-       dummy.next=list1;
-       ListNode prev=dummy;
-       for(int i=0;i<a;i++){ //move the prev node to ath node
-           prev=prev.next;
-       } 
-       ListNode curr =prev ; //move curr to the node after b
-       for(int i=a;i<=b+1;i++){
-           curr=curr.next;
-       }
-       prev.next=list2; //connect nodes before a to list2
-       while(list2.next!=null){ //find last node of list2 and connect it to the nodes after bn 
-           list2=list2.next;
-       }
-       list2.next=curr;
-       return dummy.next;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = list1;
+        ListNode prev = dummy;
+        for (int i = 0; i < a; i++) {
+            prev = prev.next;
+        }
+        
+        // Move curr to the node after 'b'
+        ListNode curr = prev;
+        for (int i = a; i <= b + 1; i++) {
+            curr = curr.next;
+        }
+        
+        // Connect the nodes before 'a' to list2
+        prev.next = list2;
+        
+        // Find the end of list2 and connect it to the nodes after 'b'
+        while (list2.next != null) {
+            list2 = list2.next;
+        }
+        list2.next = curr;
+        
+        return dummy.next;
     }
 }
