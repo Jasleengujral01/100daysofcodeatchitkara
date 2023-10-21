1977b1160ecbdcd09017295415565500c11ce136
Jasleengujral01 <122670620+Jasleengujral01@users.noreply.github.com>
2023-10-21 22:35:51 +0530

    Time: 0 ms (100%), Space: 6.1 MB (55.22%) - LeetHub

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        int prod = 1;  //initialise product by 1
-        int sum = 0;   //initialise sum by 0
-        while(n!=0 ){  //while loop till n becomes 0 and if n = 0 then we come out of the loop 
-            int digit = n%10;   //for finding last digit 
-            prod = prod * digit;
-            sum = sum + digit;
-            n = n/10;   //for removing every last digit and updating
+        int prod=1;
+        int sum=0;
+        while(n!=0){
+            int digit=n%10;
+            prod*=digit;
+            sum+=digit;
+            n/=10;
         }
-        int ans = prod - sum;   //final answer or required answer 
-        return ans;
+    return prod-sum;
+    
     }
 };
