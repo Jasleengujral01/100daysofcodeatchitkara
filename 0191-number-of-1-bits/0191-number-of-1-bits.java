247b28e2925dc1cde5f7a2aaef557a881c20357e
Jasleengujral01 <122670620+Jasleengujral01@users.noreply.github.com>
2023-10-24 11:28:54 +0530

    Time: 0 ms (100%), Space: 38.7 MB (98.71%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,12 +1,13 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        int count =0;
-        while(n!=0){
-            count+=n&1;
-            n>>>=1;
-            
-        }
-    return count;
+        int count=0;
+       while(n!=0){
+           if((n&1)==1){
+               count++;
+           }
+          n= n>>>1;
+       }
+    return count; 
     }
 }
