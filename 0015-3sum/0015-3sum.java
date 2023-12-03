1df20d82308006d2e3de033d64e1406049fbc30c
Jasleengujral01 <122670620+Jasleengujral01@users.noreply.github.com>
2023-12-03 22:45:29 +0530

    Time: 32 ms (57.08%), Space: 50.8 MB (52.82%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        // Sort the array to make it easier to find triplets
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    // Found a triplet
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    // Move pointers
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    // Move left pointer to increase the sum
+                    left++;
+                } else {
+                    // Move right pointer to decrease the sum
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
