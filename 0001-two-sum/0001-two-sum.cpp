00f0dae09602dd2014da39ec18537a6464759b40
Jasleengujral01 <122670620+Jasleengujral01@users.noreply.github.com>
2023-12-10 23:48:26 +0530

    Time: 3 ms (99.24%), Space: 11 MB (49.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,37 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-         vector<int> result(2,-1);
-        
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    result[0]=i;
-                    result[1]=j;
-                    return result;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::vector<int> result;
+
+        // Create a sorted copy of the input array
+        std::vector<int> sortedNums = nums;
+        std::sort(sortedNums.begin(), sortedNums.end());
+
+        // Use two pointers to find the pair that sums up to the target
+        int left = 0;
+        int right = sortedNums.size() - 1;
+
+        while (left < right) {
+            int sum = sortedNums[left] + sortedNums[right];
+
+            if (sum == target) {
+                // Find the indices of the pair in the original array
+                for (int i = 0; i < nums.size(); i++) {
+                    if (nums[i] == sortedNums[left] || nums[i] == sortedNums[right]) {
+                        result.push_back(i);
+                    }
                 }
+                break;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
+
         return result;
     }
 };
