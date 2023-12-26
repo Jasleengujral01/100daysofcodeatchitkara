a54b3430c1163e78dc45d24c40f92649822741d2
Jasleengujral01 <122670620+Jasleengujral01@users.noreply.github.com>
2023-12-27 00:00:10 +0530

    Time: 1 ms (100.00%), Space: 44.5 MB (13.14%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int count=0;
+        for(int i=num.length()-1;i>=0;i--){
+            if(num.charAt(i)=='0'){
+                count++;
+            }else{
+                break;
+            }
+        }
+        return num.substring(0,num.length()-count);
+    }
+}
